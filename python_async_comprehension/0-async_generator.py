929ef61a463557cf396b2f5ce47991cf577d8751
fxburpy <13808@holbertonstudents.com>
2026-06-19 23:11:03 +0400

    Update 0-async_generator.py

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 """
-Bu modül, asenkron bir jeneratör (async generator) fonksiyonu içerir.
+Bu modül, asenkron bir jeneratör fonksiyonu ve doğru tip
+tanımlamalarını (type annotations) içerir.
 """
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     10 kez döngü kurar, her seferinde asenkron olarak 1 saniye bekler
     ve 0 ile 10 arasında rastgele bir sayı döndürür (yield).
